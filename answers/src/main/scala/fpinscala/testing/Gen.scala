cd1691b6013f92394917f0723c3ed82c0306d675
Aaron Rice <adrice727@gmail.com>
2017-05-08 17:35:57 -0700

    Continue chapter 8.

--- answers/src/main/scala/fpinscala/testing/Gen.scala
+++ answers/src/main/scala/fpinscala/testing/Gen.scala
@@ -10,17 +10,38 @@ import java.util.concurrent.{Executors, ExecutorService}
 import language.postfixOps
 import language.implicitConversions
 
-trait Prop {
-  def check: Either[(FailedCase, SuccessCount), SuccessCount]
-  def &&(p: Prop): Prop = new Prop {
-    def check = Prop.this.check && p.check
-  }
-}
+case class Prop(run: (TestCases, RNG) => Result)
 
 object Prop {
   type SuccessCount = Int
   type FailedCase = String
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  type TestCases = Int
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+  case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+    def isFalsified = true
+  }
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] = Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack tract: \n ${e.getStackTrace.mkString("\n")}"
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i)}
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
 }
 
 case class Gen[A](sample: State[RNG, A]) {
@@ -28,9 +49,9 @@ case class Gen[A](sample: State[RNG, A]) {
     Gen(sample.flatMap(a => f(a).sample))
   }
 
-  def listOfN(size: Gen[Int]): Gen[List[A]] = size flatMap (n => this.listOfN(n))
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size flatMap (n => this.listOfN(Gen.unit(n)))
 
-  def listOfN(size: Int): Gen[List[A]] = Gen.listofN(n, this)
+//  def listOfN(size: Int): Gen[List[A]] = Gen.listofN(n, this)
 }
 
 object Gen {
@@ -46,6 +67,11 @@ object Gen {
   def listofN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(State.sequence(List.fill(n)(g.sample)))
 
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if (b) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.double)).flatMap(x => if (x < threshold) g1._1 else g2._1)
+  }
 
 }
 

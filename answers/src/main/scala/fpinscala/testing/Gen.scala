fb3e8338a1138cfca2b3c1d0f49f12d9f1b3031b
Aaron Rice <adrice727@gmail.com>
2017-05-24 13:29:13 -0700

    Add parallel gens.

--- answers/src/main/scala/fpinscala/testing/Gen.scala
+++ answers/src/main/scala/fpinscala/testing/Gen.scala
@@ -40,6 +40,9 @@ object Prop {
   case object Passed extends Result {
     def isFalsified = false
   }
+
+  case object Proved extends Result
+
   case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
     def isFalsified = true
   }
@@ -51,9 +54,51 @@ object Prop {
     p.run(maxSize, testCases, rng) match {
       case Falsified(msg, n) => println(s"! Falsified after $n passed tests:\n $msg")
       case Passed => println(s"+ OK, passed $testCases tests")
+      case Proved => println(s"+ OK, proved property.")
     }
 
-  def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Proved else Falsified("()", 0)
+  }
+
+  val p2 = checkPar {
+    equal(
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    )
+  }
+
+  def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] =
+    Par.map2(p, p2)(_ == _)
+
+  val p3 = check {
+    equal(
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    )(ES) get
+  }
+
+  val S = weighted(
+    choose(1, 4).map(Executors.newFixedThreadPool) -> .75,
+    unit(Executors.newCachedThreadPool) -> .25) // `a -> b` is syntax sugar for `(a,b)`
+
+  def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case (s, a) => f(a)(s).get }
+
+  def checkPar(p: Par[Boolean]): Prop =
+    forAllPar(Gen.unit(()))(_ => p)
+
+  def forAllPar2[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case (s,a) => f(a)(s).get }
+
+  def forAllPar3[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case s ** a => f(a)(s).get }
+
+  val pint = Gen.choose(0,10) map (Par.unit(_))
+  val p4 =
+    forAllPar(pint)(n => equal(Par.map(n)(y => y), n))
+
+  val forkProp = Prop.forAllPar(pint2)(i => equal(Par.fork(i), i)) tag "fork"
 
   def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] = Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
 
@@ -119,6 +164,9 @@ case class Gen[+A](sample: State[RNG, A]) {
   def **[B](g: Gen[B]): Gen[(A, B)] =
     (this map2 g) ((_, _))
 
+  lazy val pint2: Gen[Par[Int]] = choose(-100,100).listOfN(choose(0,20)).map(l =>
+    l.foldLeft(Par.unit(0))((p,i) =>
+      Par.fork { Par.map2(p, Par.unit(i))(_ + _) }))
 
   /* A method alias for the function we wrote earlier. */
   def listOfN(size: Int): Gen[List[A]] = Gen.listOfN(size, this)
@@ -128,6 +176,8 @@ case class Gen[+A](sample: State[RNG, A]) {
     size flatMap (n => this.listOfN(n))
 
   def listOf: SGen[List[A]] = Gen.listOf(this)
+
+  def listOf1: SGen[List[A]] = Gen.listOf1(this)
 
   def unsized: SGen[A] = SGen(_ => this)
 }
@@ -146,13 +196,17 @@ object Gen {
 
   def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n => g.listOfN(n))
 
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(n => g.listOfN(n max 1))
+
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if (b) g1 else g2)
 
   def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
     val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
     Gen(State(RNG.double)).flatMap(x => if (x < threshold) g1._1 else g2._1)
   }
-
+  object ** {
+    def unapply[A, B](p: (A, B)) = Some(p)
+  }
 }
 
 

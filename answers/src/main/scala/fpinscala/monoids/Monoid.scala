9443680dce5d3bd501ab10048cd44079c4f06b63
Aaron Rice <adrice727@gmail.com>
2017-07-06 14:53:33 -0700

    Continue chapter10.

--- answers/src/main/scala/fpinscala/monoids/Monoid.scala
+++ answers/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -1,11 +1,25 @@
 package fpinscala.monoids
 
-trait Monoid [A] {
+trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
 }
 
 object Monoid {
+
+  import fpinscala.testing._
+  import fpinscala.testing.Prop._
+
+  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop =
+    forAll(for {
+      x <- gen
+      y <- gen
+      z <- gen
+    } yield (x, y, z))(p =>
+      m.op(p._1, m.op(p._2, p._3)) == m.op(m.op(p._1, p._2), p._3)) &&
+      forAll(gen)((a: A) =>
+        m.op(a, m.zero) == a && m.op(m.zero, a) == a)
+
   val stringMonoid = new Monoid[String] {
     def op(a1: String, a2: String) = a1 + a2
     val zero = ""
@@ -45,6 +59,27 @@ object Monoid {
     def op(f: A => A, g: A => A) = f compose g
     val zero = (a: A) => a
   }
+
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = {
+    as.foldLeft(m.zero)((acc, a) => m.op(acc, f(a)))
+  }
+
+  def foldMap[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
+    if (as.isEmpty)
+      m.zero
+    else if (as.length == 1)
+      f(as(0))
+    else {
+      val (left, right) = as.splitAt(as.length / 2)
+      m.op(foldMap(left, m)(f), foldMap(right, m)(f))
+    }
+  }
+
+  def foldRightViafoldMap[A, B](as: List[A])(z: B)(f: (A, B) => B): B = {
+    foldMap(as, endoMonoid[B])(f.curried)(z)
+  }
+
+
 
 }
 

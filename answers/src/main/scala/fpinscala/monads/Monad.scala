79c43a00c948bbbb546f6e612366ec8869355a03
Aaron Rice <adrice727@gmail.com>
2017-07-09 11:00:13 -0700

    Monad instances.

--- answers/src/main/scala/fpinscala/monads/Monad.scala
+++ answers/src/main/scala/fpinscala/monads/Monad.scala
@@ -29,3 +29,35 @@ trait Monad[F[_]] extends Functor[F] {
   def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 }
+
+object Monad {
+  val ParMonad = new Monad[Par] {
+    def unit[A](a: => A): Par[A] = Par.unit(a)
+    def flatMap[A, B](ma: Par[A])(f: A => Par[B]): Par[B] = Par.flatMap(ma)(f)
+  }
+
+  val GenMonad = new Monad[Gen] {
+    def unit[A](a: => A): Gen[A] = Gen.unit(a)
+    def flatMap[A, B](ma: Gen[A])(f: A => Gen[B]): Gen[B] = ma flatMap f
+  }
+
+  def parserMonad[P[+-]](p: Parsers[P]) = new Monad[P] {
+    def unit[A](a: => A) = p.succeed(a)
+    def flatMap[A, B](ma: P[A])(f: A => P[B]): P[B] = p.flatMap(ma)(f)
+  }
+
+  val optionMonad = new Monad[Option] {
+    def unit[A](a: => A): Option[A] = Some(a)
+    def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] = ma flatMap f
+  }
+
+  val streamMonad = new Monad[Stream] {
+    def unit[A](a: => A): Stream[A] = Stream(a)
+    def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]): Stream[B] = ma flatMap f
+  }
+
+  val listMonad = new Monad[List] {
+    def unit[A](a: => A): List[A] = List(a)
+    def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = ma flatMap f
+  }
+}

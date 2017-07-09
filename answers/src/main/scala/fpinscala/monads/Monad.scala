0131e5a78dda6543c556fdcddac4f47dc3841e58
Aaron Rice <adrice727@gmail.com>
2017-07-08 17:06:57 -0700

    Add Monad trait.

--- answers/src/main/scala/fpinscala/monads/Monad.scala
+++ answers/src/main/scala/fpinscala/monads/Monad.scala
@@ -10,8 +10,8 @@ import language.higherKinds
 
 trait Functor[F[_]] {
   def map[A, B](fa: F[A])(f: A => B): F[B]
-  def distribute[A,B](fab: F[(A,B)]): (F[A], F[B]) = (map(fab)(_._1),map(fab)(_._2))
-  def codistribute[A,B](e: Either[F[A],F[B]]) : F[Either[A, B]] = e match {
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) = (map(fab)(_._1), map(fab)(_._2))
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
     case Left(fa) => map(fa)(Left(_))
     case Right(fb) => map(fb)(Right(_))
   }
@@ -19,7 +19,13 @@ trait Functor[F[_]] {
 
 object Functor {
   val listFunctor = new Functor[List] {
-    def map[A,B](as: List[A])(f: A => B): List[B] = as map f
+    def map[A, B](as: List[A])(f: A => B): List[B] = as map f
   }
 }
-
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](ma: F[A])(F: A => F[B]): F[B]
+  def map[A, B](fa: F[A])(f: (A) => B): F[B] = flatMap(fa)(a => unit(f(a)))
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+}

301c9b3c78071441bb75bcec28fba4ff28c5b9cf
Aaron Rice <adrice727@gmail.com>
2017-07-12 17:35:56 -0700

    Either Monad.

--- answers/src/main/scala/fpinscala/applicative/Applicative.scala
+++ answers/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -2,6 +2,7 @@ package fpinscala
 package applicative
 
 import monads.Functor
+import monads.Monad
 import state._
 import State._
 import monoids._
@@ -9,13 +10,47 @@ import language.higherKinds
 import language.implicitConversions
 
 trait Applicative[F[_]] extends Functor[F] {
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C]
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = {
+    map2(fab, fa)((f, a) => f(a))
+  }
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
+    apply(map(fa)(f.curried))(fb)
+  }
+
+  def map3[A, B, C, D](fa: F[A],
+                       fb: F[B],
+                       fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+
+  def map4[A, B, C, D, E](fa: F[A],
+                          fb: F[B],
+                          fc: F[C],
+                          fd: F[D])(f: (A, B, C, D) => E): F[E]
+  apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
   def unit[A](a: => A): F[A]
 
   def map[A,B](fa: F[A])(f: A => B): F[B] = map2(fa, unit())((a, _) => f(a))
-  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = {
-    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+
+  def sequence[A](a: List[Stream[A]]): Stream[List[A]] = {
+    ???
   }
 
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] = {
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+  }
+  def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
+
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
+
+  def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] =
+    new Monad[({type f[x] = Either[E, x]})#f] {
+      def unit[A](a: => A): Either[E, A] = Right(a)
+      override flatMap[A, B](ma: Either[E, A])(f: A => Either[E, B]) = ma match {
+        case Right(a) => f(a)
+        case Left(x) => Left(x)
+      }
+    }
+  }
 }
 
